5a246404bac26094b2611eca41f7bcaf5f980784
DM <stu9987765@gmail.com>
2025-03-07 09:37:02 +0800

    Initial Commit

---
+++ Shape/ShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeApp.swift
+//  Shape
+//
+//  Created by Mac11 on 2025/3/7.
+//
+
+import SwiftUI
+
+@main
+struct ShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
